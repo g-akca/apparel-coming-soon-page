36999167a0c8c8213399b2d53d6707826cb58a99
g-akca <gok.akca66@gmail.com>
2026-07-03 16:43:09 +0300

    Add Header styles and content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import logoImg from "/images/logo.svg";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="p-8">
+      <img src={logoImg} className="h-5" />
+    </header>
   )
 }
 
