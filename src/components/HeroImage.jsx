23f4983f8cedd25c98b5487791b23aaa60089551
g-akca <gok.akca66@gmail.com>
2026-07-05 21:13:41 +0300

    Refactor HeroImage to use picture element for responsive images, add tablet responsive styles

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -1,8 +1,16 @@
 import heroMobile from "/images/hero-mobile.jpg";
+import heroDesktop from "/images/hero-desktop.jpg";
 
 function HeroImage() {
   return (
-    <img src={heroMobile} alt="Woman with brown hair and orange shirt" />
+    <picture className="w-full">
+      <source srcSet={heroDesktop} media="(width >= 1440px)" />
+      <img
+        src={heroMobile}
+        alt="Woman with brown hair and orange shirt"
+        className="max-h-75 w-full object-cover object-top tablet:max-h-100"
+      />
+    </picture>
   )
 }
 
