627f7b6c280c7d383f5572f4a69716bf0c0d4a02
g-akca <gok.akca66@gmail.com>
2026-07-05 21:06:59 +0300

    Add tablet responsive Header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logoImg from "/images/logo.svg";
 
 function Header() {
   return (
-    <header className="p-8">
+    <header className="p-8 tablet:px-20">
       <img src={logoImg} className="h-5" />
     </header>
   )
