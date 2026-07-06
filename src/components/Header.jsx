80a6e9de4f641d53cab839b6525786fa39af47cd
g-akca <gok.akca66@gmail.com>
2026-07-06 19:15:06 +0300

    Add desktop responsive styles to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import logoImg from "/images/logo.svg";
 
 function Header() {
   return (
-    <header className="p-8 tablet:px-20">
-      <img src={logoImg} className="h-5" />
+    <header className="p-8 tablet:px-20 desktop:p-0">
+      <img src={logoImg} className="h-5 desktop:h-8" />
     </header>
   )
 }
