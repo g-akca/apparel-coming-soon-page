7dcda9e3ff8e61f8ee96a58065cd58b3123212f8
g-akca <gok.akca66@gmail.com>
2026-08-20 00:01:04 +0300

    Refactor background styles in App component and define gradient colors in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import MainSection from "./components/MainSection";
 function App() {
   return (
     <>
-      <div className="text-base leading-base text-pink-400 min-h-screen bg-linear-[140deg] from-white from-25% to-[#FFF4F4] to-80% flex flex-col desktop:hidden">
+      <div className="text-base leading-base text-pink-400 min-h-screen bg-page-gradient flex flex-col desktop:hidden">
         <Header />
 
         <main className="flex flex-col gap-16 pb-14">
@@ -16,7 +16,7 @@ function App() {
       </div>
 
       <div className="hidden desktop:grid desktop:grid-cols-[auto_610px] text-base leading-base text-pink-400 min-h-screen">
-        <main className="pl-41.25 flex flex-col gap-8 pt-20 pb-12 bg-linear-[140deg] from-white from-25% to-[#FFF4F4] to-80%">
+        <main className="pl-41.25 flex flex-col gap-8 pt-20 pb-12 bg-page-gradient">
           <Header />
 
           <MainSection />
