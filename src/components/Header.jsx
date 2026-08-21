e2410d2deeac3ef3dc29fc559195528042c1c332
g-akca <gok.akca66@gmail.com>
2026-08-21 09:57:35 +0300

    Add alt attribute to logo image in Header component for improved accessibility

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logoImg from "/images/logo.svg";
 function Header() {
   return (
     <header className="p-8 tablet:px-20 desktop:p-0">
-      <img src={logoImg} className="h-5 desktop:h-8" />
+      <img src={logoImg} alt="Base Apparel logo" className="h-5 desktop:h-8" />
     </header>
   )
 }
